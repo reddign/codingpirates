d3a6f93215898eac0f4ed403cfbb053a08fd9262
Ben <kemoib@etown.edu>
2024-08-08 09:51:34 -0400

    redid again (please work)

--- space.js
+++ space.js
@@ -254,8 +254,11 @@ function repeat() {
 }
 
 function showGameOver() {
-    context.fillStyle = "rgba(0, 0, 0, 0.7)";
-    context.fillRect(0, 0, board.width, board.height);
+  
+    context.globalAlpha = 0.5;
+    context.fillStyle = "rgba(0, 0, 0, 0.5)";
+    context.fillRect(0, 0, 900, 600);
+    context.globalAlpha = 1.0;
 
     context.fillStyle = "white";
     context.font = "60px Arial";
